0346bf24a065245c08ee6e3dd1fad011d3423878
Thai Rath <rath.thai@clublabs.com>
2021-03-05 07:03:50 -0800

    Testing github pages

---
+++ precache-manifest.89c0f5dc6443c13434a6e0308d690e01.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0aa464028cee9c3c3f262e2c333d38e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc26cb992a4ee0a62c1c",
+    "url": "./static/css/main.51dd0598.chunk.css"
+  },
+  {
+    "revision": "1c258fe157113d0df796",
+    "url": "./static/js/2.59896e39.chunk.js"
+  },
+  {
+    "revision": "6aabcba1fa695dc9c5e0085019c1988b",
+    "url": "./static/js/2.59896e39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc26cb992a4ee0a62c1c",
+    "url": "./static/js/main.8f864197.chunk.js"
+  },
+  {
+    "revision": "55dd5ca11f5853ba5453",
+    "url": "./static/js/runtime-main.8b397891.js"
+  },
+  {
+    "revision": "03ad4ec4febdbf405fa9f5dae71a54f0",
+    "url": "./static/media/aaa-california-logo.03ad4ec4.svg"
+  },
+  {
+    "revision": "47c21fb85da88ec0f87e36b284453eb4",
+    "url": "./static/media/group-12@2x.47c21fb8.png"
+  },
+  {
+    "revision": "4b35828559ed331aebc77fa0b5b7ff62",
+    "url": "./static/media/group-13.4b358285.png"
+  },
+  {
+    "revision": "82da5af4c9950057069f4938c9d9bd5a",
+    "url": "./static/media/original@3x.82da5af4.png"
+  }
+]);
